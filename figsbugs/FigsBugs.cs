7502d349ca51d5e149acf6e9a4975ac925f39bb3
ThomasBr75 <thomas.brekken@timpex.no>
2011-07-04 13:42:06 +0200

    First solution to FizzBuzz-problem

---
+++ figsbugs/FigsBugs.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FigsBugsProject
+{
+    public class FigsBugs
+    {
+        int _number;
+
+        public string Print(int number)
+        {
+            _number = number;
+            if (DivisibleBy5ButNot3())
+                return "Bugs";
+            if (DivisibleBy3And5())
+                return "FigsBugs";
+            if (DivisibleBy3ButNot5())
+                return "Figs";
+            return _number.ToString();
+        }
+
+        private bool DivisibleBy3And5()
+        {
+            return ((_number % 3 == 0) && (_number % 5 == 0));
+        }
+
+        private bool DivisibleBy5ButNot3()
+        {
+            return ((_number % 5 == 0) && (_number % 3 != 0)); 
+        }
+
+        private bool DivisibleBy3ButNot5()
+        {
+            return ((_number % 5 != 0) && (_number % 3 == 0));
+        }
+
+    }
+}
